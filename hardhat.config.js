e388d54e3ee0bdbbf3d37ee1fb6dabe8f04d957b
Tawfiq-moonHacker <54370347+Tawfiq-MoonHacker@users.noreply.github.com>
2022-04-22 12:27:00 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,14 +6,10 @@ module.exports = {
     hardhat: {
       chainId: 365
     },
-   /* mumbai: {
-      url: "HTTP://127.0.0.1:7545",
-      accounts: [process.env.pk]
-    },*/
     
     theta: {
       url: "https://eth-rpc-api-testnet.thetatoken.org/rpc",
-      accounts: ["efcbcd5915959d94eecde692655d9a03e2d7f63a662ae3e68ac68792d4ebf62d"]
+      accounts: ["private key "]
     }
   }
 };
